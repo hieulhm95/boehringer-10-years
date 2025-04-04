575afb2d27b28b2b885c39a42624f69083e86366
hieulhm95 <hieulhminh@gmail.com>
2025-04-04 14:33:41 +0700

    update transition, disable cookies set

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,32 @@ import Background from './assets/background.png';
 import LogoPreClick from './assets/pre-click-content.png';
 import LogoAfterClick from './assets/after-click-content.png';
 import AcceptButton from './assets/accept-button.png';
-import { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
+import { useState } from 'react';
+// import Cookies from 'js-cookie';
 
 function App() {
   const [isAccepted, setIsAccepted] = useState(false);
+  const [animationClass, setAnimationClass] = useState(''); // Track animation class
 
-  useEffect(() => {
-    const cookieValue = Cookies.get('boehringer_10_accepted');
-    if (cookieValue === 'true') {
-      setIsAccepted(true);
-    } else {
-      setIsAccepted(false);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const cookieValue = Cookies.get('boehringer_10_accepted');
+  //   if (cookieValue === 'true') {
+  //     setIsAccepted(true);
+  //   } else {
+  //     setIsAccepted(false);
+  //   }
+  // }, []);
 
   const handleOnClick = () => {
-    setIsAccepted(true);
-    Cookies.set('boehringer_10_accepted', 'true', { expires: 5 });
+    setAnimationClass('fade-out'); // Start fade-out animation
+
+    setTimeout(() => {
+      setIsAccepted(true);
+      setAnimationClass('fade-in'); // Start fade-in animation
+      // Cookies.set('boehringer_10_accepted', 'true', { expires: 5 });
+    }, 200);
   };
+
   return (
     <div
       className="posterWrapper"
@@ -34,12 +41,12 @@ function App() {
     >
       <div>
         {isAccepted ? (
-          <div className="content">
+          <div className={`content ${animationClass}`}>
             <img src={LogoAfterClick} alt="Logo After Click" className="logoAfterClick" />
           </div>
         ) : (
           <>
-            <div className="content">
+            <div className={`content ${animationClass}`}>
               <img src={LogoPreClick} alt="Logo Pre Click" className="logoPreClick" />
             </div>
             <div className="footer" onClick={handleOnClick}>
